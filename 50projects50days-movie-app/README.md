f65cefbfe9185dde7eb7ba1b7cb5529924abc0e4
Preston G <prestongilmore@gmail.com>
2025-05-12 19:55:01 -0600

    project complete

--- 50projects50days-movie-app/README.md
+++ 50projects50days-movie-app/README.md
@@ -1,4 +1,18 @@
 
+# Movie App 
+
+## Project Scope
+Build a movie search app using the TMDB api. 
+
+## Future Project Improvements
+- Allow users to filter movies
+- Create profiles
+- Create a watchlist
+- created a "watched" list
+- Click into a movie to view purchase options
+
+## Project Acknowledgements
+1. Project instruction and inspirtation provided by Brad Traversy and Florin Pop via their 50 Projects in 50 Days  HTML, CSS & JavaScript course found on Udemy: https://www.udemy.com/course/50-projects-50-days/?couponCode=KEEPLEARNING. 
 
 
 API Key Reference Resournes
--- 50projects50days-movie-app/script.js
+++ 50projects50days-movie-app/script.js
@@ -2,9 +2,9 @@
 // const API_KEY = process.env.API_KEY;
 // const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`
 
-
+const main = document.getElementById('main')
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b6cdfb49d3049a83c21911fde8c7d1c3&page=1'
-const IMG_PATH = 'https://imaeg.tmdb.org/t/w1280'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=b6cdfb49d3049a83c21911fde8c7d1c3&query="'
 
 const form = document.getElementById('form')
@@ -18,8 +18,44 @@ async function getMovie(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results)
+    showMovies(data.results)
 }
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                </div>
+                <div class="overview">
+                    <h3>Overview</h3>
+                    ${overview}
+                </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
