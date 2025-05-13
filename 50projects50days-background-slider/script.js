fb48445f7663c2aeb7d314839710c6031cc693cb
Preston G <prestongilmore@gmail.com>
2025-05-13 08:40:14 -0600

    arrows woking

--- 50projects50days-background-slider/script.js
+++ 50projects50days-background-slider/script.js
@@ -6,6 +6,28 @@ const rightBtn = document.getElementById('right')
 
 let activeSlide = 0
 
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if(activeSlide > slides.length - 1) {
+        activeSlide = 0
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+
+    if(activeSlide < 0) {
+        activeSlide = slides.length - 1
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
 setBgToBody()
 
 function setBgToBody() {
