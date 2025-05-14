1135e622f0f382ccaec7991d7d323a4a958ad608
Preston G <prestongilmore@gmail.com>
2025-05-14 17:39:55 -0600

    lgiht mode dark mode working

---
+++ 50projects50days-theme-clock/script.js
@@ -0,0 +1,29 @@
+
+
+const hour = document.querySelector('.hour')
+const minute = document.querySelector('.minute')
+const second = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
+
+
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark Mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light Mode'
+    }
+})
+
+
+
+
+
