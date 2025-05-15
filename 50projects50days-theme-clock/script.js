450965e01360934e296865888d5a63c25f578a22
Preston G <prestongilmore@gmail.com>
2025-05-14 18:12:56 -0600

    analog clock functionality working. Moving to digital time display.

--- 50projects50days-theme-clock/script.js
+++ 50projects50days-theme-clock/script.js
@@ -1,8 +1,8 @@
 
 
-const hour = document.querySelector('.hour')
-const minute = document.querySelector('.minute')
-const second = document.querySelector('.second')
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
@@ -22,6 +22,36 @@ toggle.addEventListener('click', (e) => {
         e.target.innerHTML = 'Light Mode'
     }
 })
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const hours = time.getHours();
+    const hoursForClock = hours % 12
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime();
+setInterval(setTime, 1000);
+
+
+
+
+
+
+
+
 
 
 
