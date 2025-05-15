b7cde480704b34768d3ab9b9a2be85bc6fff721c
Preston G <prestongilmore@gmail.com>
2025-05-14 18:29:39 -0600

    project completed

--- 50projects50days-theme-clock/script.js
+++ 50projects50days-theme-clock/script.js
@@ -9,6 +9,7 @@ const toggle = document.querySelector('.toggle')
 
 
 
+
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -27,14 +28,19 @@ function setTime() {
     const time = new Date();
     const month = time.getMonth();
     const day = time.getDay();
+    const date = time.getDate();
     const hours = time.getHours();
     const hoursForClock = hours % 12
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM'
 
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
@@ -43,6 +49,7 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
 }
 
 setTime();
+
 setInterval(setTime, 1000);
 
 
